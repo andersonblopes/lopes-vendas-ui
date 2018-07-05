a64b2ad5cef59f0ef0ab22fa50e3bf692c9e7af5
Anderson Lopes <anderson.Lopes@adtalem.com>
2018-07-05 17:37:10 -0300

    Create component venas-listagem..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { VendasListagemComponent } from './vendas-listagem/vendas-listagem.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    VendasListagemComponent
   ],
   imports: [
     BrowserModule
